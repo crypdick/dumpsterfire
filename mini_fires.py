5082862a09db6fd9db34cf98b0b5ae6cf61c6a7f
Richard Decal <richard.decal@ncf.edu>
2018-04-05 18:40:52 -0400

    relaxed constraints to get toy data

--- mini_fires.py
+++ mini_fires.py
@@ -17,7 +17,7 @@ class MRPotentialFires(MRJob):
 
     def mapper(self, _, line):
         """note: if we wanted to get better results, I think we shouldn't use midnight as the delimiter between days
-        my guess is that the minimum of edits is sometime around 5am GMT. 
+        my guess is that the minimum of edits is sometime around 5am GMT.
         to figure this out, for each revision we should just output what hr of the day it was made and plot a histogram
         """
         if "REVISION" in line[:8]:  # don't need to search whole line
@@ -47,22 +47,23 @@ class MRPotentialFires(MRJob):
             # set default val to zero if key doesn't exist yet
             # this method has good time complexity
             edit_counts[editor_id] = edit_counts.get(editor_id, 0) + edit_count
-        for editor_id, counts in edit_counts.items(): 
-            yield (date_article, (editor_id, counts))
+        for editor_id, counts in edit_counts.items():
+            if counts >= 2: # FIXME make 4 later
+                yield (date_article, (editor_id, counts))
 
     def reducer_select_w_4more_edits(self, date_article, edit_counts):
         # filter for editors who made at least 4 edits in a day in this article
-        candidates  = []
+        candidates = []
         for editor_id, counts in edit_counts:
-            if counts >= 4:
+            if counts >= 2: # FIXME make 4 later
                 candidates.append(editor_id)
-        # at least 2 editors made at least 4 edits apiece
+        # if at least 2 editors made at least 4 edits apiece
         if len(candidates) >= 2:
             # final output is a list of article_ids, with each one having a list
             # of dates which we want to grab all revision comments
             # using space as key to make it easier to strip out crud later
             yield (" ", date_article)
-    
+
 
     def steps(self):
         return [
