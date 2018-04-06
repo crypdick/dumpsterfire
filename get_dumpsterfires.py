fe35c54bf34f3051e7d0953aa99364123d202f55
Richard Decal <richard.decal@ncf.edu>
2018-04-05 22:35:16 -0400

    mostly finished, just have to choose fields we want

--- get_dumpsterfires.py
+++ get_dumpsterfires.py
@@ -6,27 +6,35 @@ import dateutil.parser
 
 class MRGrabDumpsterFires(MRJob):
 
-    # def mapper_init(self):
-    #     """we made a list of articles that are dumpsters"""
-    #     fname = "/home/shit/bin/ds_shit/distributed/dumpsterfire/results/toy_dumpsterfire.out"
-        # with open(fname, "r") as f:
-        #     """
-        #     example output from previous job:
-        #
-        #     " "	"('2001-10-04', '649')"
-        #     " "	"('2001-11-17', '632')"
-        #     " "	"('2002-01-05', '652')"
-        #     " "	"('2002-01-25', '652')"
-        #     " "	"('2002-02-02', '615')"
-        #     """
-        #     self.articleID_dates = {}
-        #     for line in f.readlines():
-        #         date_str, article_id = line.replace('"', '') \
-        #                                      .replace("'","") \
-        #                                      .replace('(', '') \
-        #                                      .replace(')','') \
-        #                                      .split(',')
-        #         self.articleID_dates[article_id] = date_str
+    def mapper_init(self):
+        """we made a list of articles that are dumpsters"""
+        fname = "/home/shit/bin/ds_shit/distributed/dumpsterfire/results/toy_dumpsterfire.out"
+        with open(fname, "r") as f:
+            """
+            example output from previous job:
+
+            " "	"('2001-10-04', '649')"
+            " "	"('2001-11-17', '632')"
+            " "	"('2002-01-05', '652')"
+            " "	"('2002-01-25', '652')"
+            " "	"('2002-02-02', '615')"
+            """
+            self.articleID_dates = {}
+            for line in f.readlines():
+                date_str, article_id = line.replace('"', '') \
+                                             .replace("'","") \
+                                             .replace('(', '') \
+                                             .replace(')','') \
+                                             .split(',')
+                if article_id in self.articleID_dates:
+                    self.articleID_dates[article_id].append(date_str)
+                else:
+                   self.articleID_dates[article_id] = [date_str]
+
+            # FIXME delete later
+            # manual injection of Anarchism articles for testing purposes
+            self.articleID_dates["12"] = ["2002-10-02", "2002-09-09", "2002-12-11",
+                                               "2002-10-28", "2002-10-03"]
 
     def mapper(self, _, line):
         """note: if we wanted to get better results, I think we shouldn't use midnight as the delimiter between days
@@ -42,67 +50,23 @@ class MRGrabDumpsterFires(MRJob):
         """
         # remove the leading 8 chars and strip decorations
         revision_info = revision_pieces[0][8:].replace('"', '').strip().split()
-        # TODO get dates
-        # remove leading 7 chars
-        comments = revision_pieces[10][7:]
-
-        yield (revision_info, comments)#str(revision_pieces[8]))
-        # yield (line[0], str(line[1:]))
+        article_id, datetimez = revision_info[0], revision_info[3]
+        # TODO get more data from revision info?
+        # if article_id in self.articleID_dates:
+        if True:  # FIXME remove in production
+            date = str(dateutil.parser.parse(datetimez).date())
+            # if date in self.articleID_dates[article_id]:
+            if True:  # FIXME remove in production
+                # get comments
+                # remove leading 7 chars
+                comments = revision_pieces[10][7:]
+                # TODO: what data do we want? do we want human-readable article title?
+                # timestamps? editor ids?
+                yield (article_id, (datetimez, comments))
 
     def reducer(self, key, values):
         for v in values:
             yield (key, v)
-        # article_id = revision_pieces[1]
-        # prefilter for articles we've whitelisted
-        # if article_id in self.filtered_datearticles:
-        #     #revision_id = revision_pieces[2]
-        #     # .date() throws out the hr-min-sec info
-        #     # can't use date as key so need to stringify first
-        #     date = str(dateutil.parser.parse(revision_pieces[4]).date())
-        #     editor_id = revision_pieces[6]
-        #     date_article = str((date, article_id))
-        #     yield (date_article, (editor_id, 1))
-
-    # def combiner(self, date_article, values):
-    #     edit_counts = {}
-    #     for (editor_id, edit_count) in values:
-    #         edit_counts[editor_id] = edit_counts.get(editor_id, 0) + edit_count
-    #     # MRjob can't pass full dictionaries (not serializable) so we gotta extract items
-    #     for editor_id, count in edit_counts.items():
-    #         yield (date_article, (editor_id, count))
-    #
-    # def reducer(self, date_article, values):
-    #     edit_counts = {}
-    #     for (editor_id, edit_count) in values:
-    #         # set default val to zero if key doesn't exist yet
-    #         # this method has good time complexity
-    #         edit_counts[editor_id] = edit_counts.get(editor_id, 0) + edit_count
-    #     for editor_id, counts in edit_counts.items():
-    #         if counts >= 1: # FIXME make 4 later
-    #             yield (date_article, (editor_id, counts))
-    #
-    # def reducer_select_w_4more_edits(self, date_article, edit_counts):
-    #     # filter for editors who made at least 4 edits in a day in this article
-    #     candidates = []
-    #     for editor_id, counts in edit_counts:
-    #         if counts >= 1: # FIXME make 4 later
-    #             candidates.append(editor_id)
-    #     # if at least 2 editors made at least 4 edits apiece
-    #     if len(candidates) >= 2:
-    #         # final output is a list of article_ids, with each one having a list
-    #         # of dates which we want to grab all revision comments
-    #         # using space as key to make it easier to strip out crud later
-    #         yield (" ", date_article)
-    #
-    #
-    # def steps(self):
-    #     return [
-    #         MRStep(mapper_init=self.mapper_init,
-    #                mapper=self.mapper,
-    #                combiner=self.combiner,
-    #                reducer=self.reducer),
-    #         MRStep(reducer=self.reducer_select_w_4more_edits)
-    #     ]
 
 
 if __name__ == "__main__":
